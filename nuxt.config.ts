1e87278b06d12b23e5310e787fd6a3ebaf4a3ac7
João Miguel <joaozinhosleepy@gmail.com>
2022-06-24 09:44:01 -0300

    wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
-    ssr: true
+    nitro: {
+        preset: 'node-server',
+        externals: {
+            inline: ['hltv','uuid'],
+          },
+    }
 })
