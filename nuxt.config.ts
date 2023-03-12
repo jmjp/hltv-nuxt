c8df0bb87c444ea2f6f2ca086205d6ab0943b16c
João Miguel <35924459+jmjp@users.noreply.github.com>
2023-03-12 18:14:32 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     nitro:{
-        preset: 'netlify'
+        preset: 'cloudflare'
     },
     experimental:{
         externalVue: true
